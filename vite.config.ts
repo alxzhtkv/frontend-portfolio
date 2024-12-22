a8a8e0825f915587f1c91cf53f2a08ea89a52921
alxzhtkv <alxzhtkv@gmail.com>
2024-12-22 23:58:31 +0300

    init

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+
+const resolvePath = (p: string) => path.resolve(__dirname, p);
+
+export default defineConfig({
+  base: '/',
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@shared": resolvePath("./src/shared"),
+      "@app": resolvePath("./src/app"),
+      "@widgets": resolvePath("./src/widgets"),
+      "@pages": resolvePath("./src/pages"),
+      "@features": resolvePath("./src/features"),
+      '@hooks': resolvePath('./src/hooks'),
+      '@assets': resolvePath('./src/assets'), 
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        additionalData: `@use "@assets/styles/globals.scss" as *;`, 
+      },
+    },
+  },
+});
