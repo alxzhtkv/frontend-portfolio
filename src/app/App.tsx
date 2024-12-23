c9c83449bf494d10037e4898f58b7e6c2e7cf038
alxzhtkv <alxzhtkv@gmail.com>
2024-12-23 17:03:14 +0300

    feat: horizontal scroll

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="test-el"></div>;
+  const mainRef = useRef<HTMLDivElement>(null);
+  const [scrollPercentage, setScrollPercentage] = useState<number>(0)
+
+  useEffect(() => {
+    function handleScroll() {
+      if (mainRef.current) {
+        const scrollPosition = mainRef.current.scrollTop;
+        const height = mainRef.current.scrollHeight;
+        const clientHeight = mainRef.current.clientHeight;
+
+        if (height - clientHeight > 0) {
+          const scrollPercentage = scrollPosition / (height - clientHeight) *100;
+          setScrollPercentage(scrollPercentage);
+        }
+      }
+    }
+
+    if (mainRef.current) {
+      mainRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    if (mainRef.current) {
+      const currentRef = mainRef.current;
+      currentRef.addEventListener("scroll", handleScroll);
+
+      return () => {
+        currentRef.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+  return (
+    <div className="main-wrapper">
+      <header className="header">
+        <div className="progress-bar">
+          <div className="progress-bar_inner" style={{height: `${scrollPercentage}%`}}></div>
+        </div>
+      </header>
+
+      <div className="horizontal-scroll-wrapper" ref={mainRef}>
+        <section>
+          <div className="first-slide">item 1</div>
+        </section>
+        <section>
+          <div className="second-slide">item 2</div>
+        </section>
+        <section>
+          {" "}
+          <div className="third-slide">item 3</div>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/hook.ts
+++
@@ -1,3 +0,0 @@
-export function hook (){
-    return ' '
-}
